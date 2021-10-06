04bb62de15ae83f89394a5f39a7c5431fb9614b7
vismarlora <vismar_lora@ucne.edu.do>
2021-10-05 23:10:06 -0400

    Creando el Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_Vismar_20190425.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Vismar_20190425.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Aportes> Aportes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\RepasoPPA.db");
+        }
+    }
+}
