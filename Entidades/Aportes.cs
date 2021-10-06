951253f49465a979fa7a2755bc523b53b0ad8b1a
vismarlora <vismar_lora@ucne.edu.do>
2021-10-05 23:07:31 -0400

    Creando la entidad Aportes

---
+++ Entidades/Aportes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Vismar_20190425.Entidades
+{
+    public class Aportes
+    {
+        [Key]
+        public int AporteId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string Persona { get; set; }
+        public string Concepto { get; set; }
+        public int Monto { get; set; }
+    }
+}
