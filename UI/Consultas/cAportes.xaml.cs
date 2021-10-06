921abf3be2e87adc27a6440114b947b6b1ec112b
vismarlora <vismar_lora@ucne.edu.do>
2021-10-05 23:39:23 -0400

    Creando el codigo de Consulta

--- UI/Consultas/cAportes.xaml.cs
+++ UI/Consultas/cAportes.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using P1_AP1_Vismar_20190425.BLL;
+using P1_AP1_Vismar_20190425.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,46 @@ namespace P1_AP1_Vismar_20190425.UI.Consultas
         public cAportes()
         {
             InitializeComponent();
+        }
+
+        public static string Conteo = "";
+        public static string Total = "";
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Aportes>();
+
+            if (!string.IsNullOrWhiteSpace(CriterioTextBox.Text))
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0: //Persona
+                        listado = AportesBLL.GetList(e => e.Persona.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+
+                    case 1: //Concepto
+                        listado = AportesBLL.GetList(e => e.Concepto.ToLower().Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = AportesBLL.GetList(c => true);
+            }
+
+            if (DesdeDatePicker.SelectedDate != null)
+                listado = AportesBLL.GetList(c => c.Fecha.Date >= DesdeDatePicker.SelectedDate);
+
+            if (HastaDatePicker.SelectedDate != null)
+                listado = AportesBLL.GetList(c => c.Fecha.Date >= HastaDatePicker.SelectedDate);
+
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+
+            Conteo = listado.Count().ToString();
+            Total = listado.Sum(c => c.Monto).ToString();
+            ConteoTextBox.Text = Conteo;
+            TotalTextBox.Text = Total;
         }
     }
 }
