91d16cae7f412b243c7f970adf1ff097fe241777
vismarlora <vismar_lora@ucne.edu.do>
2021-10-05 23:27:58 -0400

    Creando el disenio del Registro

---
+++ UI/Registros/rAportes.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P1_AP1_Vismar_20190425.UI.Registros
+{
+    /// <summary>
+    /// Interaction logic for rAportes.xaml
+    /// </summary>
+    public partial class rAportes : Window
+    {
+        public rAportes()
+        {
+            InitializeComponent();
+        }
+    }
+}
